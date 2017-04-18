39f656e86bf72a4cfbf5475827845d00d05fc643
Serhii Panchenko <spanchenko@mycredit.com.ua>
2017-04-18 16:16:56 +0200

    main page

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('.center').slick({
+        arrows: false,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }]
+    });
+    $('.js-reg-calc').slick({
+        arrows: false,
+        slidesToShow: 1
+    });
+});
