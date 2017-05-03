29b203036d9c1b88a5d05c7cfeba82e8a7199902
Serhii Panchenko <spanchenko@mycredit.com.ua>
2017-05-03 17:24:04 +0200

    03.05.2017

--- js/main.js
+++ js/main.js
@@ -2,33 +2,38 @@ $(document).ready(function() {
 
     // Calculator carousel
     $('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
         arrows: false,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        variableWidth: true,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-        }]
-    });
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
 
     // Registration carousel
     $('.js-reg-calc').slick({
-        arrows: false,
-        slidesToShow: 1
+        arrows: true,
+        slidesToShow: 1,
+        swipe: false,
+        prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve" width="20px" height="15px"><g><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225   c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z" fill="#0056B8"/></g></svg></button>',
+        nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve" width="20px" height="15px"><g><path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5   c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z" fill="#0056B8"/></g></svg></button>'
     });
 
     // Custom select
@@ -45,6 +50,76 @@ $(document).ready(function() {
     $("#card-number").mask("9999 9999 9999 9999", {
         autoclear: false,
         placeholder: "-"
+    });
+
+
+    $("#phone").mask("+38099 999 9999", {
+        autoclear: false
+    });
+
+
+
+    /*$(".tabs-title").on("click", function(e) {
+        if ($(this).hasClass("disabled")) {
+            e.preventDefault();
+            return false;
+        } else {
+            var flagform = $('#flagForm').val();
+            switch (e.currentTarget.id) {
+                case "li_1":
+                    $('#li_1').removeClass('success');
+                    if (flagform > 1) {
+                        $('#li_2').addClass('success').removeClass('is-active');
+                    }
+                    if (flagform > 2) {
+                        $('#li_3').addClass('success').removeClass('is-active');
+                    }
+                    break;
+                case "li_2":
+                    $('#li_1').addClass('success').removeClass('is-active');
+
+                    if (flagform > 1) {
+                        $('#li_2').removeClass('success');
+                    }
+                    if (flagform > 2) {
+                        $('#li_3').addClass('success').removeClass('is-active');
+                    }
+                    break;
+                case "li_3":
+                    $('#li_1').addClass('success').removeClass('is-active');
+                    $('#li_2').addClass('success').removeClass('is-active');
+                    $('#li_3').removeClass('success');
+                    break;
+            }
+        }
+    });
+    $('.js_validate [type="submit"]').on("click", function() {
+        return validate($(this).parents(".js_validate"));
+    });*/
+
+
+
+
+
+    $(function() {
+        $('#dpt').fdatepicker({
+            closeButton: false,
+            format: 'mm.dd.yyyy',
+        });
+    });
+
+
+
+
+
+
+    $('.js_validate [type="submit"]').on("click", function() {
+        return validate($(this).parents(".js_validate"));
+    });
+
+    $('.js_edit').click(function () {
+        $(this).closest('.js-reg-calc').find('button').addClass('active');
+        $(this).closest('.js-reg-calc').find('.js_edit').css('display', 'none');
     });
 
 
--- js/validate.js
+++ js/validate.js
@@ -1,139 +1,63 @@
-$(document).ready(function() {
-    $(".stepsbar a").on("click", function(e) {
-        if ($(this).hasClass("disabled")) {
-            e.preventDefault();
-            return false;
-        } else {
-            var flagform = $('#flagForm').val();
-            switch (e.currentTarget.id) {
-                case "li_a_1":
-                    $('#li_a_1').closest('li').removeClass('success');
-                    if (flagform > 1) {
-                        $('#li_a_2').closest('li').addClass('success').removeClass('active');
-                    }
-                    if (flagform > 2) {
-                        $('#li_a_3').closest('li').addClass('success').removeClass('active');
-                    }
-                    break;
-                case "li_a_2":
-                    $('#li_a_1').closest('li').addClass('success').removeClass('active');
-                    console.log(flagform);
-                    console.log($('#li_a_1').closest('li'));
-                    if (flagform > 1) {
-                        $('#li_a_2').closest('li').removeClass('success');
-                    }
-                    if (flagform > 2) {
-                        $('#li_a_3').closest('li').addClass('success').removeClass('active');
-                    }
-                    break;
-                case "li_a_3":
-                    $('#li_a_1').closest('li').addClass('success').removeClass('active');
-                    $('#li_a_2').closest('li').addClass('success').removeClass('active');
-                    $('#li_a_3').closest('li').removeClass('success');
-                    break;
-            }
-        }
-    });
-    $('.js_validate [type="submit"]').on("click", function() {
-        return validate($(this).parents(".js_validate"));
-    });
-    $('.btn-show').on("click", function(e) {
-        if (validate($(this).closest(".js_validate"))) {
-            e.preventDefault();
-            var parent = $(this).closest('.card-step');
-            parent.find('.card-action').addClass('disabled');
-            parent.next().removeClass('disabled').addClass('active');
-        }
-    });
-});
-
-function validate(form, paramId) {
+function validate(form){
     var error_class = "has-error";
     var norma_class = "has-success";
-    var item = form.find("[required]");
-    var e = 0;
-    var reg = undefined;
-    var pass = form.find('.password').val();
-    var pass_1 = form.find('.password_1').val();
-    var email = false;
-    var password = false;
-    var phone = false;
-    var card = false;
-    var passport = false;
-    var inn = false;
-    var cyrilic = false;
-
-    function mark(object, expression) {
+    var item        = form.find("[required]");
+    var e           = 0;
+    var reg         = undefined;
+    var pass        = form.find('.password').val();
+    var pass_1      = form.find('.password_1').val();
+    var email       = false;
+    var password    = false;
+    var phone       = false;
+    function mark (object, expression) {
         if (expression) {
             object.parent('div').addClass(error_class).removeClass(norma_class);
-            if (email && (object.val().length == 0)) {}
-            if (email && (object.val().length > 0)) {}
-            if (password && (object.val().length > 0)) {}
-            if (phone && (object.val().length > 0)) {}
-            if (card && (object.val().length > 0)) {}
+            if (email && (object.val().length > 0)) {
+                object.parent('div').find('.error_text').text('Incorrect Email Format');
+            }
+            if (password && (object.val().length > 0)) {
+                object.parent('div').find('.error_text').text('Password must be minimum 6 signs');
+            }
+            if (phone && (object.val().length > 0)) {
+                object.parent('div').find('.error_text').text('Incorrect phone Format');
+            }
             e++;
         } else
             object.parent('div').addClass(norma_class).removeClass(error_class);
     }
-    var idValidate = (paramId != undefined) ? '[id="' + paramId + '"]' : '';
-    form.find("[required]" + idValidate).each(function() {
-        switch ($(this).attr("data-validate")) {
+    form.find("[required]").each(function(){
+        switch($(this).attr("data-validate")) {
             case undefined:
-                mark($(this), $.trim($(this).val()).length === 0);
-                break;
+                mark ($(this), $.trim($(this).val()).length === 0);
+            break;
             case "email":
                 email = true;
                 reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-                mark($(this), !reg.test($.trim($(this).val())));
+                mark ($(this), !reg.test($.trim($(this).val())));
                 email = false;
-                break;
+            break;
             case "phone":
                 phone = true;
-                reg = /[0-9 -()+]$/;
-                mark($(this), !reg.test($.trim($(this).val())));
+                reg = /[0-9 -()+]{10}$/;
+                mark ($(this), !reg.test($.trim($(this).val())));
                 phone = false;
-                break;
+            break;
             case "pass":
                 password = true;
-                reg = /(?=^.{6,}$)(?=.*\d.*)(?=.*[A-ZА-Я].*)(?=.*[a-zа-я].*)(?=.*\W?.*).*$/;
-                mark($(this), !reg.test($.trim($(this).val())));
+                reg = /^[a-zA-Z0-9_-]{6,}$/;
+                mark ($(this), !reg.test($.trim($(this).val())));
                 password = false;
-                break;
-            case "card":
-                card = true;
-                reg = /\d\d\d\d \d\d\d\d \d\d\d\d \d\d\d\d/;
-                mark($(this), !reg.test($.trim($(this).val())));
-                card = false;
-                break;
-            case "passport":
-                passport = true;
-                reg = /\W\W\d\d\d\d\d\d/;
-                mark($(this), !reg.test($.trim($(this).val())));
-                passport = false;
-                break;
-            case "inn":
-                inn = true;
-                reg = /\d\d\d\d\d\d\d\d\d\d/;
-                mark($(this), !reg.test($.trim($(this).val())));
-                inn = false;
-                break;
+            break;
             case "pass1":
-                mark($(this), (pass_1 !== pass || $.trim($(this).val()).length === 0));
-                break;
-            case "cyrilic":
-                cyrilic = true;
-                reg = /^[ІіЇїЄєЁёҐґа-яА-Я\-\s`,\."]+$/;
-
-                mark($(this), !reg.test($.trim($(this).val())));
-                cyrilic = false;
-                break;
+                mark ($(this), (pass_1 !== pass || $.trim($(this).val()).length === 0));
+            break;
             default:
                 reg = new RegExp($(this).attr("data-validate"), "g");
-                mark($(this), !reg.test($.trim($(this).val())));
-                break
+                mark ($(this), !reg.test($.trim($(this).val())));
+            break
         }
     })
-    $('.js_valid_radio').each(function() {
+    $('.js_valid_radio').each(function(){
         var inp = $(this).find('input.required');
         var rezalt = 0;
         for (var i = 0; i < inp.length; i++) {
@@ -146,15 +70,16 @@ function validate(form, paramId) {
         }
         if (rezalt === 0) {
             $(this).addClass(error_class).removeClass(norma_class);
-            e = 1;
+            e=1;
         } else {
             $(this).addClass(norma_class).removeClass(error_class);
         }
     })
     if (e == 0) {
-        return true;
-    } else {
-        form.find(".has-error").eq(0).children().focus();
+     return true;
+    }
+    else {
+        form.find("."+error_class+" input:first").focus();
         return false;
     }
-}
+ }
